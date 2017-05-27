1bac444415f3ce9622497c673682be33ce4c4561
liuwenchao <liuwenchao@liuwenchaodeMacBook-Pro.local>
2017-05-27 15:52:51 +0800

    add apply_cancel function

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -31,7 +31,7 @@ class OrdersController < ApplicationController
     @product_lists = @order.product_lists
   end
 
-def pay_with_alipay
+  def pay_with_alipay
     @order = Order.find_by_token(params[:id])
     @order.set_payment_with!("alipay")
     @order.make_payment!
@@ -47,6 +47,13 @@ def pay_with_alipay
 
 
     redirect_to order_path(@order.token), notice: "使用微信成功完成付款"
+  end
+
+  def apply_to_cancel
+    @order = Order.find(params[:id])
+    OrderMailer.apply_cancel(@order).deliver!
+    flash[:notice] = "已提交申请"
+    redirect_to :back
   end
 
   private
